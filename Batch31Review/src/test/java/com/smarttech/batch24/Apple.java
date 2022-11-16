4fd3d7ac7896582114b0de47c19504fe60c81f6d
itadmin <itadmin@Smarttech-PC.lan>
2022-11-15 19:51:00 -0500

    this is first push to git hub

---
+++ Batch31Review/src/test/java/com/smarttech/batch24/Apple.java
@@ -0,0 +1,21 @@
+package com.smarttech.batch24;
+
+public class Apple {
+	public static void main(String[] args) {
+		
+		
+		int a= 10;
+		String b = "Jahid";
+		
+		System.out.println("Hi Java");
+		System.out.println("go");
+		System.out.println("sit");
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+	
+
+}
+
+
